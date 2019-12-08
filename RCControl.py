8793d6e49ed8fd6061e039d0bb190cd7a9ad085e
zeropt <ferr0fluidmann@gmail.com>
2019-12-08 15:05:30 -0800

    fix byte transfer

--- RCControl.py
+++ RCControl.py
@@ -3,7 +3,6 @@ from math import *
  
 import board
 import busio
-import adafruit_bme280
 from adafruit_motorkit import MotorKit
  
 # Create library object using our Bus I2C port
@@ -21,10 +20,10 @@ kr = 0.8
 kp = 0.8
 ky = 0.8
 
-data = [0, 0, 0, 0]
+data = bytearray(4)
 
 def readData():
-	i2c.readfrom_into(address, data, *, start = 0, end=len(data))
+	i2c.readfrom_into(address, data, start = 0, end=len(data))
 
 def constrain(x, Min, Max):
 	if x > Max:
@@ -38,10 +37,10 @@ def combine(Roll, Pitch, Yaw, Theta):
 	return constrain(rVal, -1.0, 1.0)
 
 while 1:
-	readData
-	roll = data[0]/127.0
-	pitch = data[1]/127.0
-	throttle = data[2]/127.0
-	yaw = data[3]/127.0
-	print roll
-	time.sleep(0.01)
+	readData()
+	roll = data[0]-127.0
+	pitch = data[1]-127.0
+	throttle = data[2]-127.0
+	yaw = data[3]-127.0
+	print(throttle)
+	time.sleep(0.1)
