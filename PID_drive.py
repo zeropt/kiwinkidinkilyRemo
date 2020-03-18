df5bdba12a4715d80dafd42be7b230179ae27345
Riley Mann <riley.mann.ctr@nps.edu>
2020-03-18 11:18:58 -0700

    move pid_controller class within pid_drive

--- PID_drive.py
+++ PID_drive.py
@@ -29,6 +29,37 @@ kP = 1.0
 kI = 0.0
 kD = 0.0
 
+class pid_controller:
+    def __init__(self, kP = 1.0, kI = 1.0, kD = 1.0, setpoint = 0.0):
+        self.kP = kP
+        self.kI = kI
+        self.kD = kD
+        self.setpoint = setpoint
+        self.out = 0.0
+        self.I = 0.0
+        self.t_prev = time.time()
+        self.e_prev = 0.0
+
+    def setSetpoint(self, setpoint):
+        self.setpoint = setpoint
+
+    def getOutput(self):
+        return self.out
+
+    def resetTime(self):
+        self.t_prev = time.time()
+
+    def update(self, sensorIn):
+        error = self.setpoint - sensorIn
+        t_delta = time.time() - self.t_prev #use time
+        #t_delta = 1.0; #don't use time
+        P = self.kP * error
+        self.I += self.kI * error * t_delta
+        D = self.kD * (error - self.e_prev)/t_delta
+        self.out = P+self.I+D
+        self.t_prev = time.time()
+        self.e_prev = error
+
 pid = pid_controller(kP, kI, kD, setpoint)
 
 data = bytearray(4)
@@ -73,34 +104,3 @@ while 1:
     kit.motor3.throttle = combine(roll, pitch, yaw, 5.0*pi/6.0)
     kit.motor4.throttle = combine(roll, pitch, yaw, 5.0*pi/2.0)
     time.sleep(0.1)
-
-class pid_controller:
-    def __init__(self, kP = 1.0, kI = 1.0, kD = 1.0, setpoint = 0.0):
-        self.kP = kP
-        self.kI = kI
-        self.kD = kD
-        self.setpoint = setpoint
-        self.out = 0.0
-        self.I = 0.0
-        self.t_prev = time.time()
-        self.e_prev = 0.0
-
-    def setSetpoint(self, setpoint):
-        self.setpoint = setpoint
-
-    def getOutput(self):
-        return self.out
-
-    def resetTime(self):
-        self.t_prev = time.time()
-
-    def update(self, sensorIn):
-        error = self.setpoint - sensorIn
-        t_delta = time.time() - self.t_prev #use time
-        #t_delta = 1.0; #don't use time
-        P = self.kP * error
-        self.I += self.kI * error * t_delta
-        D = self.kD * (error - self.e_prev)/t_delta
-        self.out = P+self.I+D
-        self.t_prev = time.time()
-        self.e_prev = error
