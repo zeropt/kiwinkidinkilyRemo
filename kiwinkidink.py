2bceef98674e925bc7288913c1dadc4c805df009
Riley Mann <riley.mann.ctr@nps.edu>
2020-03-19 13:55:19 -0700

    servo control

--- kiwinkidink.py
+++ kiwinkidink.py
@@ -1,11 +1,36 @@
 import logging
 import time
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import RPi.GPIO as GPIO
 
 log = logging.getLogger('RemoTV.hardware.kiwinkidink')
 
+mh = None
+
+camServoMin = 6.0
+camServoMax = 11.5
+camServo = 8.0
+
+def incrementCamServo(amount):
+    camServo += amount
+    
+    log.debug("cam servo positions:", camServo)
+    
+    if camServo > camServoMax:
+        camServo = camServoMax
+    if camServo < camServoMin:
+        camServo = camServoMin
+    cs.ChangeDutyCycle(camServo)
+
 def setup(robot_config):
-    # your hardware setup code goes here
-    return
+    global mh
+    global cs
+    global camServo
+
+    mh = Adafruit_MotorHAT(addr=0x60)
+    GPIO.setup(18, GPIO.OUT)
+    cs = GPIO.PWM(18, 50)
+    cs.start(camServo)
 
 def move(args):
     command=args['button']['command']
@@ -24,3 +49,9 @@ def move(args):
     elif command == 'R':
         # Your hardware movement code for right goes here
         return
+    elif command == 'u':
+        incrementCamServo(-0.2)
+        time.sleep(0.02)
+    elif command == 'd':
+        incrementCamServo(0.2)
+        time.sleep(0.02)
