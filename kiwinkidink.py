d3fcc7eeafe417a9b62653fa190d699c059ee88e
zeropt <ferr0fluidmann@gmail.com>
2020-03-19 13:39:59 -0700

    fix indentation

--- kiwinkidink.py
+++ kiwinkidink.py
@@ -4,18 +4,23 @@ import time
 log = logging.getLogger('RemoTV.hardware.kiwinkidink')
 
 def setup(robot_config):
-  # your hardware setup code goes here
+    # your hardware setup code goes here
+    return
 
 def move(args):
-  command=args['button']['command']
-  
-  log.debug("move kiwinkidink command : %s", command)
+    command=args['button']['command']
 
-  if command == 'F':
-    # Your hardware movement code for forward goes here
-  elif command == 'B':
-    # Your hardware movement code for backwards goes here
-  elif command == 'L':
-    # Your hardware movement code for left goes here
-  elif command == 'R':
-    # Your hardware movement code for right goes here
+    log.debug("move kiwinkidink command : %s", command)
+
+    if command == 'F':
+        # Your hardware movement code for forward goes here
+        return
+    elif command == 'B':
+        # Your hardware movement code for backwards goes here
+        return
+    elif command == 'L':
+        # Your hardware movement code for left goes here
+        return
+    elif command == 'R':
+        # Your hardware movement code for right goes here
+        return
